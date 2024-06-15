807b9683a44a00b21077f6c78ecc5a5de5158ac8
BRANDON <brandonturcios@unitec.edu>
2024-06-14 21:29:58 -0600

    evaluador

---
+++ ProyectoLenguajes/Evaluador.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+bool esOperador(char c);
+int precedencia(char c);
+int evaluarPostfija(const std::string& expresion);
+
